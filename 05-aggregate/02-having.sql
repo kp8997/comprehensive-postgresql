e52aa3730689751eecf74dd9a659d8f3720b4e49
Khang Phan <invisibledark3@gmail.com>
2026-05-26 23:26:38 +0700

    ađ more having count

--- 05-aggregate/02-having.sql
+++ 05-aggregate/02-having.sql
@@ -2,3 +2,8 @@ SELECT photo_id, COUNT(*) FROM comments
 WHERE photo_id < 3
 GROUP BY photo_id
 HAVING COUNT(*) > 2;
+
+SELECT user_id, COUNT(*) FROM comments
+WHERE photo_id < 50
+GROUP BY user_id
+HAVING COUNT(*) > 20;
