3e9a7b024216a2b13ac9ee1ff9652c69206d0316
Khang Phan <invisibledark3@gmail.com>
2026-05-27 17:01:02 +0700

    add more examples for correlated query

--- 09-subquery/04-correlated.sql
+++ 09-subquery/04-correlated.sql
@@ -5,3 +5,8 @@ WHERE p1.price = (
     WHERE p1.department = p2.department
 );
 
+-- Show the number of order of each product 
+SELECT name, (
+    SELECT COUNT(*) FROM orders o1
+    WHERE o1.product_id = p1.id
+) as num_orders FROM products p1;
