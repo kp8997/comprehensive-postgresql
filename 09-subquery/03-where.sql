d675d198127c873cf1d1d7d63180577d0fd01bf2
Khang Phan <invisibledark3@gmail.com>
2026-05-27 14:36:06 +0700

    add example for subquery on where clause

---
+++ 09-subquery/03-where.sql
@@ -0,0 +1,10 @@
+SELECT id FROM orders
+WHERE product_id IN (
+    SELECT id FROM products
+    WHERE price / weight > 50
+);
+
+SELECT name FROM products
+WHERE price > (
+    SELECT AVG(price) FROM products
+);
