d717d1475be0a353c59a26a83841f697355aa4e8
Khang Phan <invisibledark3@gmail.com>
2026-05-27 14:46:04 +0700

    add more example for where with new operator (IN, NOT IN, >, <

--- 09-subquery/03-where.sql
+++ 09-subquery/03-where.sql
@@ -8,3 +8,15 @@ SELECT name FROM products
 WHERE price > (
     SELECT AVG(price) FROM products
 );
+
+SELECT name, department from products
+where department NOT IN (
+    SELECT department FROM products
+    WHERE price < 100
+);
+
+SELECT name, department, price from products
+WHERE price > (
+    SELECT MAX(price) FROM products
+    WHERE department = 'Industrial'
+);
