601cbdfcc61f00de450436dd13c2b606df634311
Khang Phan <invisibledark3@gmail.com>
2026-05-27 13:50:37 +0700

    add avg calculation for dataset of many to many

--- 09-subquery/02-many-many.sql
+++ 09-subquery/02-many-many.sql
@@ -3,3 +3,9 @@ FROM (
     SELECT name, price / weight as price_weight_ratio FROM products
 )
 WHERE price_weight_ratio > 5;
+
+-- Average number of order per user of all users
+SELECT AVG(num_orders) from (
+    SELECT user_id, COUNT(*) as num_orders from orders
+    GROUP BY user_id
+);
