be3bab580ab805e42383eb77507e36e980c5dd80
Khang Phan <invisibledark3@gmail.com>
2026-05-27 15:04:43 +0700

    alternative with > all instead of > max

--- 09-subquery/03-where.sql
+++ 09-subquery/03-where.sql
@@ -1,22 +1,35 @@
+-- show the id of orders that have price / weight > 50
 SELECT id FROM orders
 WHERE product_id IN (
     SELECT id FROM products
     WHERE price / weight > 50
 );
 
+-- show the name of product that have price greater than average price of product
 SELECT name FROM products
 WHERE price > (
     SELECT AVG(price) FROM products
 );
 
-SELECT name, department from products
+-- show the name, department of product that have price not in department of product that have price less than 100
+SELECT name, department FROM products
 where department NOT IN (
     SELECT department FROM products
     WHERE price < 100
 );
 
-SELECT name, department, price from products
+-- show the name, department, price of product that have price greater than max price of product in Industrial department
+
+SELECT name, department, price FROM products
 WHERE price > (
     SELECT MAX(price) FROM products
+    WHERE department = 'Industrial'
+);
+
+-- alternative
+
+SELECT name, department, price FROM products
+WHERE price > ALL (
+    SELECT price FROM products
     WHERE department = 'Industrial'
 );
