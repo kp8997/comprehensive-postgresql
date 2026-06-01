8eff09a6d5ec2900427aa17986b7f18b46f69e18
Khang Phan <invisibledark3@gmail.com>
2026-06-01 21:10:49 +0700

    add tables for design

--- 14-design/02-mention.sql
+++ 14-design/02-mention.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(50),
-    password VARCHAR(255),
+    password VARCHAR(255)
 )
 
 CREATE TABLE posts (
@@ -13,4 +13,35 @@ CREATE TABLE posts (
     updated_at TIMESTAMP DEFAULT NOW(),
     lat REAL,
     lng REAL
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    contents VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATA TABLE likes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+    x INTEGER,
+    y INTEGER
+);
+
+CREATE TABLE caption_tags (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
 );
