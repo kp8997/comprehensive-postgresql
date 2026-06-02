3bf3949e521d78b9f4a777a26e3e8f61eb217cc3
Khang Phan <invisibledark3@gmail.com>
2026-06-03 03:33:37 +0700

    add another exercise

--- 15-complex-query/01-query.sql
+++ 15-complex-query/01-query.sql
@@ -2,3 +2,8 @@
 SELECT * FROM users 
 ORDER BY id DESC 
 LIMIT 3;
+
+-- all post of user with id 200, only show caption and username
+SELECT p.caption, u.username FROM posts p
+JOIN users u ON u.id = p.user_id
+WHERE p.user_id = 200;
