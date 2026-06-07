8a01ef138b0e0cec30fecaa0efa26aaf99f7b31c
Khang Phan <invisibledark3@gmail.com>
2026-06-07 17:34:22 +0700

    add some support command

--- 16-internal-component/02-index.sql
+++ 16-internal-component/02-index.sql
@@ -9,3 +9,8 @@ SELECT pg_size_pretty(pg_relation_size('users'));
 
 -- index heap file
 SELECT relname, relkind FROM pg_class WHERE relkind = 'i';
+
+-- extension to query page inside index file
+CREATE EXTENSION pageinspect;
+
+SELECT * FROM bt_metap('users_username_idx');
