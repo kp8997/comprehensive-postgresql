c50d86b9f32ad4bcb1938156273d8b6e03d00129
Khang Phan <invisibledark3@gmail.com>
2026-06-07 17:06:38 +0700

    update note

--- 16-internal-component/02-index.sql
+++ 16-internal-component/02-index.sql
@@ -2,3 +2,7 @@ CREATE INDEX users_username_idx ON users (username);
 
 -- DROP INDEX users_username_idx;
 EXPLAIN ANALYZE SELECT * FROM users WHERE username = 'Emil30';
+
+SELECT pg_size_pretty(pg_relation_size('users_username_idx'));
+
+SELECT pg_size_pretty(pg_relation_size('users'));
