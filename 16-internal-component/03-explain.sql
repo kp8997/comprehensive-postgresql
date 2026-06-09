c645d02c407eaad43880e065c903a383157c3c04
Khang Phan <invisibledark3@gmail.com>
2026-06-09 20:43:51 +0700

    add command

--- 16-internal-component/03-explain.sql
+++ 16-internal-component/03-explain.sql
@@ -9,3 +9,6 @@ WHERE username = 'Alyson14';
 EXPLAIN ANALYZE SELECT username, contents FROM users 
 JOIN comments ON users.id = comments.user_id
 WHERE username = 'Alyson14';
+
+SELECT * FROM pg_stats WHERE tablename = 'users';
+ 
