752a78c0d2dbb5dcd5ce3b18fc83f11e827ae36c
Khang Phan <invisibledark3@gmail.com>
2026-06-11 03:02:47 +0700

    initialize the transaction section

---
+++ 19-transaction/01-create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE accounts (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    balance NUMERIC NOT NULL DEFAULT 0
+);
+
+INSERT INTO accounts (id, name, balance)
+VALUES 
+    (1, 'Alice', 100),
+    (2, 'Bob', 100),
+    (3, 'Charlie', 300);
+
+SELECT * FROM accounts;
