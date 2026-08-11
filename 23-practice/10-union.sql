a6e4ac7cfb52f9d8d496f719148396e899bf17df
Khang Phan <invisibledark3@gmail.com>
2026-08-11 19:33:48 +0700

    add simple union

---
+++ 23-practice/10-union.sql
@@ -0,0 +1,6 @@
+-- You, for some reason, want a combined list of all surnames and all facility names. 
+-- Yes, this is a contrived example :-). Produce that list!
+
+(select name as Stibbons from cd.facilities)
+UNION
+(select surname as Stibbons from cd.members);
