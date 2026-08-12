81493da6c4fefea09d3c73f239c3d88be6788bc6
Khang Phan <invisibledark3@gmail.com>
2026-08-13 00:48:25 +0700

    add new answer for mix exercise

---
+++ 23-practice/p4-aggregate/40-mix-max-group-with-having-list.sql
@@ -0,0 +1,24 @@
+-- Output the facility id that has the highest number of slots booked.
+-- For bonus points, try a version without a LIMIT clause.
+-- This version will probably look messy!
+
+select b.facid, sum(b.slots) as sum_by_slots
+  from cd.bookings b
+  group by b.facid
+  having sum(b.slots) = (
+	select max(sum_slots) 
+	from (select sum(slots) as sum_slots from cd.bookings group by facid)
+  )
+  order by facid
+;
+
+-- answer
+
+select facid, sum(slots) as totalslots
+	from cd.bookings
+	group by facid
+	having sum(slots) = (select max(sum2.totalslots) from
+		(select sum(slots) as totalslots
+		from cd.bookings
+		group by facid
+		) as sum2);
