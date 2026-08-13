3652618072e9ab31c9052c1667911dd0bf81941a
Khang Phan <invisibledark3@gmail.com>
2026-08-13 14:32:56 +0700

    add aggregate earliest time in group

---
+++ 23-practice/p4-aggregate/43-aggragate-booking-condition.sql
@@ -0,0 +1,22 @@
+-- Produce a list of each member name, id, and their first booking after September 1st 2012. Order by member ID.
+
+select m.surname,
+  m.firstname,
+  m.memid,
+  min(b.starttime)
+from cd.members m
+  join cd.bookings b on b.memid = m.memid
+where b.starttime >= '2012-09-01'
+group by surname,
+  firstname,
+  m.memid
+order by memid;
+
+-- answer
+select mems.surname, mems.firstname, mems.memid, min(bks.starttime) as starttime
+	from cd.bookings bks
+	inner join cd.members mems on
+		mems.memid = bks.memid
+	where starttime >= '2012-09-01'
+	group by mems.surname, mems.firstname, mems.memid
+order by mems.memid;
