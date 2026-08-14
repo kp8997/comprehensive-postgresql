b7fabc977dd33ffbe8cfd50a3808e4a9f7f92a9c
Khang Phan <invisibledark3@gmail.com>
2026-08-14 15:08:02 +0700

    add an exercise of round and rank function

--- 23-practice/p4-aggregate/47-aggregate-rank-member.sql
+++ 23-practice/p4-aggregate/47-aggregate-rank-member.sql
@@ -2,3 +2,39 @@
 --  rounded to the nearest ten hours. 
 -- Rank them by this rounded figure, producing output of first name, surname, rounded hours, rank.
 -- Sort by rank, surname, and first name.
+select firstname,
+  surname,
+  hours,
+  rank () over (
+    order by hours desc
+  )
+from (
+    select m.firstname,
+      m.surname,
+      ROUND(ROUND(sum(b.slots) / 2, 0) / 10) * 10 as hours
+    from cd.members m
+      join cd.bookings b on b.memid = m.memid
+    group by surname,
+      surname
+  )
+order by rank,
+  firstname,
+  surname
+-- answer
+select firstname,
+  surname,
+  hours,
+  rank() over (
+    order by hours desc
+  )
+from (
+    select firstname,
+      surname,
+      ((sum(bks.slots) + 10) / 20) * 10 as hours
+    from cd.bookings bks
+      inner join cd.members mems on bks.memid = mems.memid
+    group by mems.memid
+  ) as subq
+order by rank,
+  surname,
+  firstname;
