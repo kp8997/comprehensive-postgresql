da073005aedbe46ba6c0a056139b78855692e014
Khang Phan <invisibledark3@gmail.com>
2026-08-14 23:31:05 +0700

    add time * slots to find endtime of booking

---
+++ 23-practice/p5-date/59-end-time-of-booking.sql
@@ -0,0 +1,17 @@
+-- Return a list of the start and end time of the last 10 bookings (ordered by the time at which they end,
+-- followed by the time at which they start) in the system.
+select starttime,
+  starttime + (slots * interval '30 minutes') as endtime
+from cd.bookings b
+order by endtime desc,
+  starttime desc
+limit 10;
+
+-- answer
+
+select starttime,
+  starttime + slots *(interval '30 minutes') endtime
+from cd.bookings
+order by endtime desc,
+  starttime desc
+limit 10
