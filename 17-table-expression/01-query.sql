049be46882536870fa1ed10b0dfbc91ab03313e4
Khang Phan <invisibledark3@gmail.com>
2026-08-14 16:54:46 +0700

    add minor modification

--- 17-table-expression/01-query.sql
+++ 17-table-expression/01-query.sql
@@ -7,7 +7,7 @@ JOIN (
 WHERE t.created_at < '2010-01-07';
 
 
-WITH AS tags (
+WITH tags AS (
     SELECT user_id, created_at FROM caption_tags
     UNION ALL
     SELECT user_id, created_at FROM photo_tags
