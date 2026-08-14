29db8cc2bb3a4141a9ff054cf77db9ebcb8825e6
Khang Phan <invisibledark3@gmail.com>
2026-08-15 01:29:46 +0700

    add lpad with zip code

---
+++ 23-practice/p6-string/66-pad-zipcode.sql
@@ -0,0 +1,9 @@
+-- The zip codes in our example dataset have had leading zeroes removed from them by virtue of being stored as a numeric type.
+-- Retrieve all zip codes from the members table, padding any zip codes less than 5 characters long with leading zeroes.
+-- Order by the new zip code.
+
+select lpad(zipcode::char(5), 5, '0') as zip from cd.members
+
+-- answer
+
+select lpad(cast(zipcode as char(5)),5,'0') zip from cd.members order by zip;
