480e2b50b887717fbd4a5a36416438800f530bc4
Khang Phan <invisibledark3@gmail.com>
2026-08-14 22:16:56 +0700

    practice with generate_series()

---
+++ 23-practice/p5-date/54-list-of-date.sql
@@ -0,0 +1,5 @@
+-- Produce a list of all the dates in October 2012.
+-- They can be output as a timestamp (with time set to midnight) or a date.
+
+
+select generate_series(timestamp '2012-10-01', '2012-10-31', '1 day'::interval);
