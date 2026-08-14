ee36a658049f04054b5c1b736b052b5e5af0bbc6
Khang Phan <invisibledark3@gmail.com>
2026-08-14 16:01:31 +0700

    add more note

--- 23-practice/p4-aggregate/49-classify-facilities-revenue.sql
+++ 23-practice/p4-aggregate/49-classify-facilities-revenue.sql
@@ -23,6 +23,13 @@ from (
       name asc
   ) 
   
+-- note that ntile() divide into N groups equally, based on the order by
+-- in this case, it divides the facilities into 3 groups based on their revenue
+-- and the facility with the highest revenue will be in the first group
+
+-- remember order in ntile over will give the number desc or asc
+-- we have to sort 2 more layers when reaching the right answer
+
 -- answer
 select name, case when class=1 then 'high'
 		when class=2 then 'average'
