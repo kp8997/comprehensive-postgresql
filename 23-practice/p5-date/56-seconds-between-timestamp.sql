3538f8fa647d62843fdc53073d16fbb44c8ed257
Khang Phan <invisibledark3@gmail.com>
2026-08-14 22:29:44 +0700

    epoch type to extract seconds

---
+++ 23-practice/p5-date/56-seconds-between-timestamp.sql
@@ -0,0 +1,12 @@
+-- Work out the number of seconds between the timestamps '2012-08-31 01:00:00' and '2012-09-02 00:00:00'
+
+select round(extract(epoch from (timestamp '2012-09-02 00:00:00' - '2012-08-31 01:00:00')));
+
+-- answer
+
+select 	extract(day from ts.int)*60*60*24 +
+	extract(hour from ts.int)*60*60 + 
+	extract(minute from ts.int)*60 +
+	extract(second from ts.int)
+	from
+		(select timestamp '2012-09-02 00:00:00' - '2012-08-31 01:00:00' as int) ts
