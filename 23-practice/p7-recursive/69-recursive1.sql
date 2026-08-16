a01e0671130816b511ea856fd0328193d533d5da
Khang Phan <invisibledark3@gmail.com>
2026-08-16 15:44:59 +0700

    add an exercise recursive

---
+++ 23-practice/p7-recursive/69-recursive1.sql
@@ -0,0 +1,3 @@
+-- Find the upward recommendation chain for member ID 27: that is, the member who recommended them, 
+--  and the member who recommended that member, and so on.
+-- Return member ID, first name, and surname. Order by descending member id.
